50a5df37cdd809b93ada2940bae1df4f7e8c934f
Daniel Waters <dantownanduptheroad@hotmail.com>
2013-06-10 22:23:53 +0100

    Initial commit of blank project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BSG::Application.config.secret_token = '99271bab717b5f985da7c109b2b3cd677342945f75a3b1ee9c8978b918ff24c558d0fca8cee6814ea53c4da4f20758c95e4221b6511d3bece4d26c1f18cd3dbe'
